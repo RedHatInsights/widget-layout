c0e665c699e7827fee56b5f74594f0d703c977ea
ewinchel <ewinchel@redhat.com>
2024-03-04 09:49:43 -0500

    Add Widget card styling fix

--- src/Components/WidgetDrawer/WidgetDrawer.tsx
+++ src/Components/WidgetDrawer/WidgetDrawer.tsx
@@ -30,7 +30,7 @@ const WidgetWrapper = ({ title, widgetType }: React.PropsWithChildren<{ title: s
   const setDropInItem = useSetAtom(currentDropInItemAtom);
   const headerActions = (
     <Tooltip content={<p>Move widget</p>}>
-      <Icon>
+      <Icon className="pf-v5-u-pt-md">
         <GripVerticalIcon style={{ fill: '#6a6e73' }} />
       </Icon>
     </Tooltip>
@@ -55,7 +55,7 @@ const WidgetWrapper = ({ title, widgetType }: React.PropsWithChildren<{ title: s
       unselectable="on"
       draggable={true}
     >
-      <CardHeader actions={{ actions: headerActions }}>
+      <CardHeader className="pf-v5-u-py-md" actions={{ actions: headerActions }}>
         <CardTitle>{title}</CardTitle>
       </CardHeader>
     </Card>
