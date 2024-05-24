e5ced297f77b66bc03e19c4b1d07abb34a6e869e
epwinchell <1287144+epwinchell@users.noreply.github.com>
2024-05-23 20:56:55 -0400

    Styling fixes (#89)
    * Styling fixes
    
    * Styling fixes

--- src/Routes/Default/Default.tsx
+++ src/Routes/Default/Default.tsx
@@ -71,7 +71,7 @@ const DefaultRoute = (props: { layoutType?: LayoutTypes }) => {
       <Portal notifications={notifications} removeNotification={removeNotification} />
       <Header />
       <AddWidgetDrawer dismissible={false}>
-        <PageSection className="widg-c-page__main-section--grid pf-v5-u-p-md pf-v5-u-p-lg-on-sm">
+        <PageSection className="widg-c-page__main-section--grid pf-v5-u-p-md-on-sm">
           <GridLayout isLayoutLocked={isLayoutLocked} {...props} />
         </PageSection>
       </AddWidgetDrawer>
